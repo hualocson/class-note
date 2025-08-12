13ad13427d8946800ad48a00e36882bd51f758f1
locson <hualocson@gmail.com>
2025-08-12 20:04:49 +0700

    feat(class-sessions): create ClassSessionsPage component to wrap main session content

---
+++ src/app/class-sessions/page.tsx
@@ -0,0 +1,11 @@
+import MainClassSessionsPage from "./components/main";
+
+const ClassSessionsPage = () => {
+  return (
+    <div className="bg-background text-foreground min-h-screen">
+      <MainClassSessionsPage />
+    </div>
+  );
+};
+
+export default ClassSessionsPage;
