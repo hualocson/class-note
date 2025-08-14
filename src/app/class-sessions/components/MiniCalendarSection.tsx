a7ebeafade4e2d3687cea38fe0e2c63eccafa7f0
locson <hualocson@gmail.com>
2025-08-14 19:43:23 +0700

    feat(mini-calendar): add MiniCalendarSection component for date selection

---
+++ src/app/class-sessions/components/MiniCalendarSection.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { FC, useState } from "react";
+
+import { startOfWeek } from "date-fns";
+
+import {
+  MiniCalendar,
+  MiniCalendarDays,
+  MiniCalendarNavigation,
+  MiniCalendarWeekDay,
+} from "@/components/ui/shadcn-io/mini-calendar";
+
+interface IMiniCalendarSectionProps {
+  selectedDate: Date;
+  onSelectedDateChange: (date: Date) => void;
+}
+
+const MiniCalendarSection: FC<IMiniCalendarSectionProps> = ({
+  selectedDate,
+  onSelectedDateChange,
+}) => {
+  const [startDate, setStartDate] = useState(
+    startOfWeek(selectedDate, { weekStartsOn: 1 })
+  );
+
+  const onStartDateChange = (date: Date | undefined) => {
+    if (date) {
+      setStartDate(date);
+    } else {
+      setStartDate(startOfWeek(selectedDate, { weekStartsOn: 1 }));
+    }
+  };
+
+  return (
+    <section className="mt-2 flex w-full items-center justify-center">
+      <MiniCalendar
+        value={selectedDate}
+        onValueChange={(date) => onSelectedDateChange(date ?? new Date())}
+        days={7}
+        startDate={startDate}
+        onStartDateChange={onStartDateChange}
+      >
+        <MiniCalendarNavigation direction="prev" />
+        <MiniCalendarDays>
+          {(date) => (
+            <MiniCalendarWeekDay date={date} key={date.toISOString()} />
+          )}
+        </MiniCalendarDays>
+        <MiniCalendarNavigation direction="next" />
+      </MiniCalendar>
+    </section>
+  );
+};
+
+export default MiniCalendarSection;
