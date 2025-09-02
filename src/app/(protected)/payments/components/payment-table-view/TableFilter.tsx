a05789247d14b8d7eed2c4a322c54926c3dfed04
locson <hualocson@gmail.com>
2025-09-02 15:42:48 +0700

    feat(payments): add filters for payment status and class in payment table view

---
+++ src/app/(protected)/payments/components/payment-table-view/TableFilter.tsx
@@ -0,0 +1,34 @@
+import { GetClassesSuccessResponseData } from "@/actions/types";
+import { PaymentStatus } from "@/enums";
+import { FacetedFilter } from "@tanstack/react-table";
+
+import { PaymentType } from "./type";
+
+const paymentStatusFilter: FacetedFilter<PaymentType> = {
+  key: "status",
+  singleData: {
+    label: "Status",
+    options: [
+      { value: PaymentStatus.PENDING, label: "Pending" },
+      { value: PaymentStatus.PAID, label: "Paid" },
+      { value: PaymentStatus.CANCELLED, label: "Cancelled" },
+    ],
+    multiple: true,
+  },
+};
+
+const genClassFilter: (
+  classes: GetClassesSuccessResponseData["rows"]
+) => FacetedFilter<PaymentType> = (classes) => ({
+  key: "class",
+  singleData: {
+    label: "Class",
+    options: classes.map((classItem) => ({
+      value: classItem.id,
+      label: classItem.name,
+    })),
+  },
+  multiple: true,
+});
+
+export { genClassFilter, paymentStatusFilter };
